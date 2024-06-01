892cf5d2018a77ff2a0e05355b6dd820abe794d1
ffaayy301515 <minffaayy301515@gmail.com>
2024-06-01 16:57:46 +0800

    try catch 大法好

--- assets/Scripts/MultiRoom.ts
+++ assets/Scripts/MultiRoom.ts
@@ -44,62 +44,76 @@ export class MultiRoom extends Component {
     private roomRef = null;
 
     start() {
-        this.roomRef = firebase.database().ref('rooms/' + MultiRoom.roomID.toString());
+        try {
+            this.roomRef = firebase.database().ref('rooms/' + MultiRoom.roomID.toString());
 
-    // instantiate player frames
-        for(let i=1; i<=4; i++){
-            const playerFrame = instantiate(this.userPrefab);
-            playerFrame.setPosition(-100, 160-100*(i-1));
-            playerFrame.getChildByName("name").getComponent(Label).string = "Wait...";
-            playerFrame.getChildByName("KD").getComponent(Label).string = "";
-            this.playerFrameList.push(playerFrame);
-            this.node.addChild(playerFrame);
-        }
-
-        this.roomIDLabel.string = MultiRoom.roomID.toString();
-        this.returnButton.node.on(Node.EventType.MOUSE_UP, () => {
-            MultiRoom.roomID = 0;
-            const user = firebase.auth().currentUser;
-            this.userList.splice(this.userList.indexOf(user.uid), 1);
-            let userCnt = 0;
-            this.roomRef.once('value', (snapshot) => {
-                userCnt = snapshot.val().userCnt - 1;
-                if(userCnt == 0) this.roomRef.remove();
-                else this.roomRef.update({userCnt: userCnt, users: this.userList});
-            }, (e) => {
-                console.log(e);
-            });
-            
-            director.loadScene("MultiSelect");
-        });
-        this.roomRef.off();
-        this.roomRef.on('value', (snapshot) => {
-            if (!snapshot.exists()) return;
-
-            const room = snapshot.val();
-            this.roomName.string = room.roomName;
-            this.userList = room.users;
-            this.map = room.map;
-            this.mode = room.mode;
-            this.userCnt = room.userCnt;
-
-            console.log("userList");
-            console.log(this.userList);
-            for(let i=0; i<4; i++){
-                if(i > this.userCnt) continue;
-                    
-                const userRef = firebase.database().ref('users/' + this.userList[i+1]);
-                userRef.once('value', (snapshot) => {
-                    if (!snapshot.exists()) return;
-                    console.log(snapshot.val());
-                    const user = snapshot.val();
-                    this.playerFrameList[i].getChildByName("name").getComponent(Label).string = user.name;
-                    this.playerFrameList[i].getChildByName("KD").getComponent(Label).string = (user.death == 0 ? "NA" : (user.kill / user.death).toString());
-                    
-                }); 
+        // instantiate player frames
+            for(let i=1; i<=4; i++){
+                const playerFrame = instantiate(this.userPrefab);
+                playerFrame.setPosition(-100, 160-100*(i-1));
+                playerFrame.getChildByName("name").getComponent(Label).string = "Wait...";
+                playerFrame.getChildByName("KD").getComponent(Label).string = "";
+                this.playerFrameList.push(playerFrame);
+                this.node.addChild(playerFrame);
             }
-            
-        });
+
+            this.roomIDLabel.string = MultiRoom.roomID.toString();
+            this.returnButton.node.on(Node.EventType.MOUSE_UP, () => {
+                MultiRoom.roomID = 0;
+                const user = firebase.auth().currentUser;
+                this.userList.splice(this.userList.indexOf(user.uid), 1);
+                let userCnt = 0;
+                this.roomRef.once('value', (snapshot) => {
+                    userCnt = snapshot.val().userCnt - 1;
+                    if(userCnt == 0) this.roomRef.remove();
+                    else this.roomRef.update({userCnt: userCnt, users: this.userList});
+                }, (e) => {
+                    console.log(e);
+                });
+                
+                director.loadScene("MultiSelect");
+            });
+            this.roomRef.off();
+            this.roomRef.on('value', (snapshot) => {
+                if (!snapshot.exists()) return;
+                try{
+                    const room = snapshot.val();
+                    this.roomName.string = room.roomName;
+                    this.userList = room.users;
+                    this.map = room.map;
+                    this.mode = room.mode;
+                    this.userCnt = room.userCnt;
+
+                    console.log("userList");
+                    console.log(this.userList);
+                    for(let i=0; i<4; i++){
+                        if(i > this.userCnt) continue;
+                            
+                        const userRef = firebase.database().ref('users/' + this.userList[i+1]);
+                        userRef.once('value', (snapshot) => {
+                            try{
+                                if (!snapshot.exists()) return;
+                                console.log(snapshot.val());
+                                const user = snapshot.val();
+                                this.playerFrameList[i].getChildByName("name").getComponent(Label).string = user.name;
+                                this.playerFrameList[i].getChildByName("KD").getComponent(Label).string = (user.death == 0 ? "NA" : (user.kill / user.death).toString());
+                                
+                            }
+                            catch(e){
+                                console.log(e);
+                            }
+                            
+                        }); 
+                    }
+                }
+                catch(e){
+                    console.log(e);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 
     protected onDestroy(): void {
