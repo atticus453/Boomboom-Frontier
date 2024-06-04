bee7983e24192e5bb6ce8aa99aadf74e8fb89725
ffaayy301515 <minffaayy301515@gmail.com>
2024-06-04 22:19:36 +0800

    ranking, select room fixed

--- assets/Scripts/Ranking.ts
+++ assets/Scripts/Ranking.ts
@@ -7,11 +7,6 @@ export default class Ranking extends cc.Component {
     @property(cc.Prefab)
     item_prefab: cc.Prefab = null;
 
-    // @property(cc.JsonAsset)
-    // playerDataJson: cc.JsonAsset = null;
-
-    // LIFE-CYCLE CALLBACKS:
-
     onLoad () {
         var scr = this.node.getChildByName("RankingScroll").getComponent(cc.ScrollView);
         var usersRef = firebase.database().ref('users');
@@ -38,23 +33,6 @@ export default class Ranking extends cc.Component {
     
             }
         });
-        
-        // let data = this.playerDataJson.json;
-        // data.Player.sort((a, b)=>{
-        //     return a.score - b.score;
-        // });
-        // cc.log(data.Player);
-        
-        // for(var i = 0; i < data.Player.length; i++) {
-        //     var optItem = cc.instantiate(this.item_prefab); //實例化一個預製體
-        //     var optItemSize = optItem.getComponent(cc.UITransform).contentSize;
-        //     cc.log(optItemSize.width);
-        //     scr.content.addChild(optItem);
-        //     optItem.position = cc.v3(optItem.position.x + 200, optItem.position.y - i*optItemSize.height - 50, 0);
-        //     optItem.getChildByName("name").getComponent(cc.Label).string = data.Player[i].name;
-        //     optItem.getChildByName("score").getComponent(cc.Label).string = data.Player[i].score;
-
-        // }
     }
 
     start () {
